f502eb9433d24e4c4541e400d305cacc7fd596fb
Jean-Marc Barozet <jbarozet@gmail.com>
2024-01-03 11:09:52 +0100

    fix catppuccin plugin not loaded

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,18 @@
 return {
-  -- add catppuccin
-  {
-    "catppuccin", -- Make sure to use "catppuccin" and not "catppuccin/nvim"
-    opts = {
-      style = "mocha",
-      transparent_background = false,
-    },
-  },
-  -- add tokyonight
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "moon" },
-  },
+	-- add catppuccin
+	{
+		"catppuccin/nvim", -- Make sure to use "catppuccin" and not "catppuccin/nvim"
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			style = "mocha",
+			transparent_background = false,
+		},
+	},
+	-- add tokyonight
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = { style = "moon" },
+	},
 }
