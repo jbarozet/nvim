5d74ff9a6ad2db5d929e3558761f328da3e1aa3f
Jean-Marc Barozet <jbarozet@gmail.com>
2024-01-13 09:25:22 +0100

    adding keymap for neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,18 +6,19 @@ vim.g.loaded_netrwPlugin = 1
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function ()
-    require('neo-tree').setup {}
-    vim.keymap.set('n', '<leader>ee', ':Neotree toggle<CR>', { desc = "Toggle file explorer" })
-    vim.keymap.set('n', '<leader>ec', ':Neotree close<CR>', {})
-    vim.keymap.set('n', '<leader>et', ':Neotree current<CR>', {})
-    vim.keymap.set('n', '<leader>eb', ':Neotree buffers reveal float<CR>', {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setuv({})
+		vim.keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", { desc = "Toggle file explorer" })
+		vim.keymap.set("n", "<leader>er", ":Neotree filesystem reveal left<CR>", { desc = "Reveal file explorer left" })
+		vim.keymap.set("n", "<leader>ex", ":Neotree close<CR>", {})
+		vim.keymap.set("n", "<leader>et", ":Neotree current<CR>", {})
+		vim.keymap.set("n", "<leader>eb", ":Neotree buffers reveal float<CR>", {})
+	end,
 }
