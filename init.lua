0d234f7b3ada4c71c0667e04c432fffa7ef9eda2
Jean-Marc Barozet <jbarozet@gmail.com>
2024-02-16 15:13:28 +0100

    add custom ollama prompt

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
@@ -12,40 +11,43 @@ vim.g.maplocalleader = ' '
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
-  install = {
-    colorscheme = { "nightfly" },
-  },
-  checker = {
-    enabled = true,
-    notify = false, -- do not notify. Updates are displayed in lualine
-  },
-  change_detection = {
-    notify = false,
-  },
+	install = {
+		colorscheme = { "nightfly" },
+	},
+	checker = {
+		enabled = true,
+		notify = false, -- do not notify. Updates are displayed in lualine
+	},
+	change_detection = {
+		notify = false,
+	},
 })
-
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
-require('config.settings')
+require("config.settings")
 
 -- [[ Basic Keymaps ]]
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 
-require('config.keymaps')
+require("config.keymaps")
 
+require("gen").prompts["DevOps me!"] = {
+	prompt = "You are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, kubernetes, python. You answer with code examples when possible. $input:\n$text",
+	replace = true,
+}
