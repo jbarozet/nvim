9333b6a8f64eb6ed74ed7e370053990083e2b495
Jean-Marc Barozet <jbarozet@gmail.com>
2024-03-10 08:52:40 +0100

    fixed error on ansible linter - this is ansible_lint and not ansible-lint in nvim-lint config

--- lua/plugins/lsp/linting.lua
+++ lua/plugins/lsp/linting.lua
@@ -1,18 +1,33 @@
 return {
 	"mfussenegger/nvim-lint",
-	-- Event to trigger linters
-	events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-	linters_by_ft = {
-		python = { "flake8" },
-		markdown = { "markdownlint" },
-		ansible = { "ansible-lint" },
-		yaml = { "yamllint" },
-		terraform = { "tflint" },
-		lua = { "luacheck" },
-		javascript = { "eslint_d" },
-		typescript = { "eslint_d" },
-		javascriptreact = { "eslint_d" },
-		typescriptreact = { "eslint_d" },
-		svelte = { "eslint_d" },
+	event = {
+		"BufReadPre",
+		"BufNewFile",
 	},
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			python = { "flake8" },
+			-- python = { "pylint" },
+			markdown = { "markdownlint" },
+			ansible = { "ansible_lint" }, -- NOTE: make sure to use ansible_lint and not ansible-lint
+			yaml = { "yamllint" },
+			terraform = { "tflint" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
 }
