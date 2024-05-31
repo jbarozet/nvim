fda2b3fc6d639c58577aeae2292eb2fb9710f0e0
Jean-Marc Barozet <jbarozet@gmail.com>
2024-05-31 11:41:15 +0200

    change neo-tree to floating mode - sinplifies keymap to just popup file-tree an reveal current buffer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -16,16 +16,18 @@ return {
 	config = function()
 		require("neo-tree").setup({
 			popup_border_style = "rounded",
-			-- window = { position = "float", width = 30 },
+			window = { position = "float", width = 30 },
 		})
 
 		-- vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal float<CR>", { desc = "Reveal file explorer float" })
 		-- vim.keymap.set("n", "<leader>e", ":Neotree filesystem float<CR>", { desc = "Toggle file explorer (float)" })
 
-		vim.keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", { desc = "Toggle file explorer" })
-		vim.keymap.set("n", "<leader>er", ":Neotree filesystem reveal left<CR>", { desc = "Reveal file explorer left" })
-		vim.keymap.set("n", "<leader>ex", ":Neotree close<CR>", {})
-		vim.keymap.set("n", "<leader>et", ":Neotree current<CR>", {})
-		vim.keymap.set("n", "<leader>eb", ":Neotree buffers reveal float<CR>", {})
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal float<CR>", { desc = "Reveal file explorer left" })
+
+		-- vim.keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", { desc = "Toggle file explorer" })
+		-- vim.keymap.set("n", "<leader>er", ":Neotree filesystem reveal left<CR>", { desc = "Reveal file explorer left" })
+		-- vim.keymap.set("n", "<leader>ex", ":Neotree close<CR>", {})
+		-- vim.keymap.set("n", "<leader>et", ":Neotree current<CR>", {})
+		-- vim.keymap.set("n", "<leader>eb", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
