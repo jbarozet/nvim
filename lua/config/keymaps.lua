08acb9e452dc943903999f2bec352e4dee74eb5b
Jean-Marc Barozet <jbarozet@gmail.com>
2024-09-23 18:37:13 +0200

    update keymaps for notes and window resize

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,9 +9,12 @@ keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
 keymap.set("n", "x", '"_x')
 
+-- Oil
+keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+
 -- Go to Notes
-keymap.set("n", "<leader>nt", ":cd ~/Notes/Tech | e ,\n", { desc = "load technical notes" })
-keymap.set("n", "<leader>nt", ":cd ~/Notes/Personal | e PLANNING.md\n", { desc = "load personal notes" })
+keymap.set("n", "<leader>nt", ":cd ~/Notes/Tech | e .\n", { desc = "load technical notes" })
+keymap.set("n", "<leader>np", ":cd ~/Notes/Personal | e inbox/01_PLANNING.md\n", { desc = "load personal notes" })
 keymap.set("n", "<leader>nc", ":cd ~/Notes/Cisco | e PLANNING.md\n", { desc = "load Cisco notes" })
 
 -- better up/down
@@ -21,8 +24,8 @@ keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent
 keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+-- keymap.set("n", "+", "<C-a>")
+-- keymap.set("n", "-", "<C-x>")
 
 -- better indenting
 keymap.set("v", "<", "<gv")
@@ -42,19 +45,21 @@ keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true }
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
--- Resize window using <ctrl> arrow keys
-keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+-- Resize window
+keymap.set("n", "=", "<cmd>horizontal resize +2<cr>", { desc = "Increase window height" })
+keymap.set("n", "-", "<cmd>horizontal resize -2<cr>", { desc = "Decrease window height" })
+keymap.set("n", "+", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+keymap.set("n", "_", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Split window
--- keymap.set("n", "ss", ":split<Return>", opts)
--- keymap.set("n", "sv", ":vsplit<Return>", opts)
+keymap.set("n", "sv", "<C-w>v", opts) -- split window vertically (:vsplit)
+keymap.set("n", "ss", "<C-w>s", opts) -- split window horizontally (:split)
+keymap.set("n", "sx", "<C-W>c", opts) -- close current split window (<cmd>close<CR>)
+keymap.set("n", "se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sx", "<C-w>c", { desc = "Close current split" }) -- close current split window
 
 -- buffers
 keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
@@ -74,7 +79,7 @@ keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- o
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "]t", "cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "]t", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "[t", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
