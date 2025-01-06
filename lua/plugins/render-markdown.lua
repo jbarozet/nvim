d334bef788d41ccc4a68a11d1f197364ea22ec6a
Jean-Marc Barozet <jbarozet@gmail.com>
2025-01-06 09:26:41 +0100

    adding markdown rendering

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,17 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      sign = false,
+      right_pad = 1,
+    },
+    heading = {
+      enabled = true,
+      sign = true,
+      position = "overlay",
+      -- width = "block",
+      border = false,
+      icons = {},
+    },
+  },
+}
