aa2806dae8568b91291a61af8d2d62fdeda01c7a
Jean-Marc Barozet <jbarozet@gmail.com>
2025-04-16 09:24:48 +0200

    updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,9 @@
 -- Add any additional keymaps here
 
 -- vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+-- vim.keymap.set("n", "<C-c>", "ciw")
+-- vim.keymap.set("n", "<Up>", "<c-w>k")
+-- vim.keymap.set("n", "<Down>", "<c-w>j")
+-- vim.keymap.set("n", "<Left>", "<c-w>h")
+-- vim.keymap.set("n", "<Right>", "<c-w>l")
