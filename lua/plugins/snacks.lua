c6ae8af8b8bd8f9319500e3334c62a7584a3e814
Jean-Marc Barozet <jbarozet@gmail.com>
2025-04-25 08:00:52 +0200

    adding snacks keymaps

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,114 @@
+-- https://github.com/folke/snacks.nvim/blob/main/docs/lazygit.md
+-- https://github.com/folke/snacks.nvim/blob/main/docs/picker.md
+-- https://github.com/folke/snacks.nvim/blob/main/docs/image.md
+
+-- NOTE: If you experience an issue in which you cannot select a file with the
+-- snacks picker when you're in insert mode, only in normal mode, and you use
+-- the bullets.vim plugin, that's the cause, go to that file to see how to
+-- resolve it
+-- https://github.com/folke/snacks.nvim/issues/812
+
+return {
+  "folke/snacks.nvim",
+  keys = {
+    -- Iterate through incomplete tasks in Snacks_picker
+    {
+      -- -- You can confirm in your teminal lamw26wmal with:
+      -- -- rg "^\s*-\s\[ \]" test-markdown.md
+      "<leader>ti",
+      function()
+        Snacks.picker.grep({
+          prompt = " ",
+          -- pass your desired search as a static pattern
+          search = "^\\s*- \\[ \\]",
+          -- we enable regex so the pattern is interpreted as a regex
+          regex = true,
+          -- no “live grep” needed here since we have a fixed pattern
+          live = false,
+          -- restrict search to the current working directory
+          dirs = { vim.fn.getcwd() },
+          -- include files ignored by .gitignore
+          args = { "--no-ignore" },
+          -- Start in normal mode
+          on_show = function()
+            vim.cmd.stopinsert()
+          end,
+          finder = "grep",
+          format = "file",
+          show_empty = true,
+          supports_live = false,
+          layout = "ivy",
+        })
+      end,
+      desc = "[i] Search for incomplete tasks",
+    },
+    -- Iterate throuth completed tasks in Snacks_picker lamw26wmal
+    {
+      "<leader>tc",
+      function()
+        Snacks.picker.grep({
+          prompt = " ",
+          -- pass your desired search as a static pattern
+          search = "^\\s*- \\[x\\]",
+          -- we enable regex so the pattern is interpreted as a regex
+          regex = true,
+          -- no “live grep” needed here since we have a fixed pattern
+          live = false,
+          -- restrict search to the current working directory
+          dirs = { vim.fn.getcwd() },
+          -- include files ignored by .gitignore
+          args = { "--no-ignore" },
+          -- Start in normal mode
+          on_show = function()
+            vim.cmd.stopinsert()
+          end,
+          finder = "grep",
+          format = "file",
+          show_empty = true,
+          supports_live = false,
+          layout = "ivy",
+        })
+      end,
+      desc = "[c] Search for complete tasks",
+    },
+    -- List git branches with Snacks_picker to quickly switch to a new branch
+    {
+      "<leader>ga",
+      function()
+        Snacks.picker.git_branches({
+          layout = "select",
+        })
+      end,
+      desc = "Branches",
+    },
+    -- Navigate my buffers
+    {
+      "<leader>,",
+      function()
+        Snacks.picker.buffers({
+          -- I always want my buffers picker to start in normal mode
+          on_show = function()
+            vim.cmd.stopinsert()
+          end,
+          finder = "buffers",
+          format = "buffer",
+          hidden = false,
+          unloaded = true,
+          current = true,
+          sort_lastused = true,
+          win = {
+            input = {
+              keys = {
+                ["d"] = "bufdelete",
+              },
+            },
+            list = { keys = { ["d"] = "bufdelete" } },
+          },
+          -- In case you want to override the layout for this keymap
+          -- layout = "ivy",
+        })
+      end,
+      desc = "[P]Snacks picker buffers",
+    },
+  },
+}
